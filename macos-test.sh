bf61e30c80ce4637338d9231d37cb5d719c2bdae
Madhav Ramdin <madhav.ramdin@q4inic.com>
2021-04-13 10:24:35 -0400

    adding subdomains

--- macos-test.sh
+++ macos-test.sh
@@ -6,56 +6,49 @@ GREEN='\033[0;32m'
 #Testing TCP ports 
 echo "######## Q4inc VSM Test for macOS ########"
 echo "### All lines below should be in green ###"
-nc -vz tokbox.com 443 &>/dev/null
+nc -vz hlg.tokbox.com 443 &>/dev/null
 if [ $? = 0 ]
 then
   echo -e "${GREEN}TCP connection to tokbox succesful!${NC}"
 else 
   echo -e "${RED}TCP connection to tokbox failed!${NC}"
 fi
-#nc -vz opentok.com 443 &>/dev/null 
-#if [ $? = 0 ]
-#then
-#  echo -e "${GREEN}TCP connection to opentok succesful!${NC}"
-#else 
-#  echo -e "${RED}TCP connection to opentok failed!${NC}"
-#fi
-nc -vz events.q4inc.com 443 &>/dev/null
+nc -vz api.events.q4inc.com 443 &>/dev/null
 if [ $? = 0 ]
 then
   echo -e "${GREEN}TCP connection to events.q4inc.com succesful!${NC}"
 else 
   echo -e "${RED}TCP connection to events.q4inc.com failed!${NC}"
 fi
-nc -vz twilio.com 443 &>/dev/null
+nc -vz api.twilio.com 443 &>/dev/null
 if [ $? = 0 ]
 then
   echo -e "${GREEN}TCP connection to twilio succesful!${NC}"
 else 
   echo -e "${RED}TCP connection to twilio failed!${NC}"
 fi
-nc -vz callstats.io 443 &>/dev/null
+nc -vz stats-eu-west-1.callstats.io 443 &>/dev/null
 if [ $? = 0 ]
 then
   echo -e "${GREEN}TCP connection to callstats succesful!${NC}"
 else 
   echo -e "${RED}TCP connection to callstats failed!${NC}"
 fi
-nc -vz pendo.io 443 &>/dev/null
+nc -vz data.pendo.io 443 &>/dev/null
 if [ $? = 0 ]
 then
   echo -e "${GREEN}TCP connection to pendo.io succesful!${NC}"
 else 
   echo -e "${RED}TCP connection to pendo.io failed!${NC}"
 fi
-nc -vz launchdarkly.com 443 &>/dev/null
+nc -vz app.launchdarkly.com 443 &>/dev/null
 if [ $? = 0 ]
 then
   echo -e "${GREEN}TCP connection to launchdarkly succesful!${NC}"
 else 
   echo -e "${RED}TCP connection to launchdarkly failed!${NC}"
 fi
-nc -vz sentry.io 443 &>/dev/null
+nc -vz o485366.ingest.sentry.io 443 &>/dev/null
 if [ $? = 0 ]
 then
   echo -e "${GREEN}TCP connection to sentry.io succesful!${NC}"
@@ -65,20 +58,14 @@ fi
 
 #Testing UDP ports
 
-nc -vzu tokbox.com 3478 &>/dev/null
+nc -vzu hlg.tokbox.com 3478 &>/dev/null
 if [ $? = 0 ]
 then
   echo -e "${GREEN}UDP connection to opentok succesful!${NC}"
 else 
   echo -e "${RED}UDP connection to opentok failed!${NC}"
 fi
-#nc -vzu opentok.com 3478 &>/dev/null
-#if [ $? = 0 ]
-#then
-#  echo -e "${GREEN}UDP connection to opentok succesful!${NC}"
-#else 
-#  echo -e "${RED}UDP connection to opentok failed!${NC}"
-#fi
+
 
 
 
